bdd03110eac88ccf17c9fb4d043ded7178c36f6d
ferdi-nan <100830066+Ferdi-nan@users.noreply.github.com>
2024-04-15 11:26:34 +0200

    Add files via upload

---
+++ assets/js/beweg-script.js
@@ -0,0 +1,18 @@
+/*
+function myFunction() {
+  var txt;
+  if (confirm("Press a button!")) {
+    txt = ">ou pressed OK!"}
+			else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+
+
+//==============================================
+$("document").ready( function () {
+	        alert("folgende Übung ist nur für große Bildschirme geeignet (Tablett oder größer)");
+	    }); 
+
+	   */
